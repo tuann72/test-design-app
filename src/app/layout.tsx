37652c82523c1885e3aa8a63aac426852dd1aaa2
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-25 01:24:37 -0500

    intial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const pop = Poppins({weight: "500", subsets: ["latin"]})
 
 export const metadata: Metadata = {
   title: "Create Next App",
