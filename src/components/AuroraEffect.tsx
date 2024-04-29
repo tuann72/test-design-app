68d68ddd54ea2f4fcc72e9b4bca037cec4f7e129
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-29 17:07:05 -0500

    Changed landing page to light mode to fit with the rest of the site.

--- src/components/AuroraEffect.tsx
+++ src/components/AuroraEffect.tsx
@@ -8,7 +8,7 @@ const COLORS: string[] = ["#14e81e", "#00ea8d", "#017ed5", "#b53dff", "#8d00c4"]
 
 export const AuroraEffect = ({slogan, text, btnText, location}:{slogan:string, text:string, btnText:string, location:string}) => {
     const color = useMotionValue(COLORS[0]);
-    const backgroundImage = useMotionTemplate`radial-gradient(125% 100% at 50% 0%, #020617 50%, ${color})`;
+    const backgroundImage = useMotionTemplate`radial-gradient(125% 100% at 50% 0%, #f5f5f5 75%, ${color})`;
     const border = useMotionTemplate`1px solid ${color}`;
     const boxShadow = useMotionTemplate`0px 4px 24px ${color}`;
 
@@ -28,10 +28,10 @@ export const AuroraEffect = ({slogan, text, btnText, location}:{slogan:string, t
         style={{
             backgroundImage
         }}
-        className="relative grid min-h-screen w-screen place-content-center overflow-hidden bg-gray-950 px-4 py-24 text-gray-200">
+        className="relative grid min-h-screen w-screen place-content-center overflow-hidden bg-slate-100 px-4 py-24 text-gray-950">
             <div className="flex flex-col items-center">
                 <img className="h-30 w-80" src="/logoipsum-235.svg"/>
-                <h1 className="max-w-6xl text-5xl font-medium text-center pb-3">{slogan}</h1>
+                <h1 className="max-w-6xl text-5xl font-medium bg-gradient-to-b from-slate-400 to-slate-800 bg-clip-text text-transparent text-center pb-3">{slogan}</h1>
                 <p className="text-center pb-6">{text}</p>
                 <motion.button
                     className="group relative gap-1 flex w-fit px-4 py-2 rounded-md"
